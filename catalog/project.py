0805b7be9e3c54583597410388f5b7fdd6651309
sabrinadowla14 <sabrinadowla14@gmail.com>
2017-08-09 18:10:38 -0500

    updated for linux configuration project

--- catalog/project.py
+++ catalog/project.py
@@ -17,7 +17,7 @@ from flask import abort
 app = Flask(__name__)
 
 CLIENT_ID = json.loads(
-    open('client_secret_value.json', 'r').read())['web']['client_id']
+    open(r'/var/www/catalog/catalog/client_secret_value.json', 'r').read())['web']['client_id']
 APPLICATION_NAME = "Shops for Women"
 
 
@@ -49,10 +49,10 @@ def fbconnect():
     access_token = request.data
     print "access token received %s " % access_token
 
-    app_id = json.loads(open('fb_client_secrets.json', 'r').read())[
+    app_id = json.loads(open(r'/var/www/catalog/catalog/fb_client_secrets.json', 'r').read())[
         'web']['app_id']
     app_secret = json.loads(
-        open('fb_client_secrets.json', 'r').read())['web']['app_secret']
+        open(r'/var/www/catalog/catalog/fb_client_secrets.json', 'r').read())['web']['app_secret']
     url = ('https://graph.facebook.com/v2.8/oauth/access_token?'
            'grant_type=fb_exchange_token&client_id=%s&client_secret=%s'
            '&fb_exchange_token=%s') % (app_id, app_secret, access_token)
